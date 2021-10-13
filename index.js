9657f9bd4c64e44d04e568d1c26d1dfa2c6dc327
Tani Kaye <1tanikaye@gmail.com>
2021-10-13 10:12:05 -0400

    commit tree 2ad119a01473eb65dece8fe3ecd3ff7ce7d76d17

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+var saturdayFun = function (input) {
+  if (input !== undefined) {
+    return `This Saturday, I want to ${input}!`
+  } else {
+    return "This Saturday, I want to roller-skate!"
+  }
+}
+
+var mondayWork = function (input) {
+  if (input !== undefined) {
+    return `This Monday, I will ${input}.`
+  } else {
+    return "This Monday, I will go to the office."
+  }
+}
+
+function wrapAdjective (mainInput = '*') {
+  return function (input = 'special') {
+return `You are ${mainInput}${input}${mainInput}!`;
+  }
+}
